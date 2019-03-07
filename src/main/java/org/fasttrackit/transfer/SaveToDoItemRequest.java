4af1c448e76c50065ba08ef5c1d1a64d8c340136
Adina Bucur <adinaa.bucur@gmail.com>
2019-03-07 21:31:39 +0200

    Initial commit

---
+++ src/main/java/org/fasttrackit/transfer/SaveToDoItemRequest.java
@@ -0,0 +1,42 @@
+package org.fasttrackit.transfer;
+
+import java.sql.Date;
+
+public class SaveToDoItemRequest {
+
+    private String description;
+    private Date deadline;
+    private boolean done;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(Date deadline) {
+        this.deadline = deadline;
+    }
+
+    public boolean isDone() {
+        return done;
+
+
+    }
+
+    @Override
+    public String toString() {
+        return "SaveToDoItemRequest{" +
+                "description='" + description + '\'' +
+                ", deadline=" + deadline +
+                ", done=" + done +
+                '}';
+    }
+}
+
